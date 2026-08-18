207f3c1c471d0571a353def532d203cea1be9cdb
ST10259792-Muzzi <st10259792@myemeris.edu.za>
2026-08-18 13:14:21 +0200

    Person 3
    Completed and tested,
    
    > insy7314-g3-2026-part1-hustlehub@1.0.0 start
    > node server.js
    
    Server running on port 5000

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "HustleHub+ API Running"
+    });
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
